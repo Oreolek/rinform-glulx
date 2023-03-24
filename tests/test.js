9164b6bbfb8743cf965bd18f159bcd5a4651008d
Alexander Yakovlev <keloero@oreolek.me>
2023-03-24 18:58:23 +0700

    WIP

--- tests/test.js
+++ tests/test.js
@@ -1,24 +1,18 @@
 const spawn	= require('child_process').spawn;
-//const fs = require('fs');
 const assert = require('chai').assert;
 
 function cleanString(input) {
-  return input.replace(/[^а-яА-Я\.\?\! ]/g, '').replace("\r", "\n").trim()
+  return input.replace(/[^а-яА-ЯЁё\.\?\! >]/g, '').trim()
 }
 
-describe('Тест игры', function () {
-  it('Игра должна соответствовать прохождению', function (done) {
-    const glk = 'ambiguity.ulx';
-    this.game = spawn('/usr/bin/glulxe-term', [glk]);
-    this.game.stdin.setEncoding = 'utf-8';
-    this.out = "";
-    this.game.stdout.on('data', function(data){
-      let out = data.toString()+"\n"
-      out = cleanString(out);
-      console.log(out)
-      assert.equal(out, 'eee');
-      done()
-    })
-    this.game.stdin.write("взять кошку\nконец\nда\n\n");
-  });
-});
+const glk = 'ambiguity.ulx';
+this.game = spawn('/usr/bin/glulxe-term', [glk]);
+this.game.stdin.setEncoding = 'utf-8';
+this.game.stdout.on('data', function(data){
+  const out = cleanString(data.toString()).split(/\r?\n/)
+  console.log(out)
+  assert.equal(out, '');
+})
+this.game.stdin.write("взять кошку\n");
+this.game.stdin.write("взять себя\n");
+this.game.stdin.write("конец\nда\n\n");
