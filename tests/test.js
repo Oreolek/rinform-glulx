f18bf2aa3d728b57bd57796b75e0659203e062b6
Alexander Yakovlev <keloero@oreolek.me>
2023-03-24 19:28:30 +0700

    compilation step

--- tests/test.js
+++ tests/test.js
@@ -1,11 +1,14 @@
 const spawn	= require('child_process').spawn;
+const execSync	= require('child_process').execSync;
 const assert = require('chai').assert;
 
 function cleanString(input) {
   return input.replace(/[^а-яА-ЯЁё\.\?\! >]/g, '').trim()
 }
 
+
 const glk = 'ambiguity.ulx';
+execSync('inform', ['+../../library,../../libext', '+language_name=Russian', '-DG', '-Cu', '$DICT_CHAR_SIZE=4', '_Sources/ambiguity.inf']);
 this.game = spawn('/usr/bin/glulxe-term', [glk]);
 this.game.stdin.setEncoding = 'utf-8';
 this.game.stdout.on('data', function(data){
