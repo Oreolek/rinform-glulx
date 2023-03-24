6fca659662dee0ce8a105e6eddb4c8af65bf5d59
Alexander Yakovlev <keloero@oreolek.me>
2023-03-24 19:31:54 +0700

    Quixe

--- tests/test.js
+++ tests/test.js
@@ -1,21 +1,16 @@
-const spawn	= require('child_process').spawn;
 const execSync	= require('child_process').execSync;
+const quixe = require('quixe');
 const assert = require('chai').assert;
 
 function cleanString(input) {
   return input.replace(/[^а-яА-ЯЁё\.\?\! >]/g, '').trim()
 }
 
-
-const glk = 'ambiguity.ulx';
+const Q = new quixe(2)
 execSync('inform', ['+../../library,../../libext', '+language_name=Russian', '-DG', '-Cu', '$DICT_CHAR_SIZE=4', '_Sources/ambiguity.inf']);
-this.game = spawn('/usr/bin/glulxe-term', [glk]);
-this.game.stdin.setEncoding = 'utf-8';
-this.game.stdout.on('data', function(data){
-  const out = cleanString(data.toString()).split(/\r?\n/)
-  console.log(out)
-  assert.equal(out, '');
-})
-this.game.stdin.write("взять кошку\n");
-this.game.stdin.write("взять себя\n");
-this.game.stdin.write("конец\nда\n\n");
+Q.init('ambiguity.ulx', function(text) {
+  console.log(text)
+});
+Q.input("взять кошку");
+Q.input("взять себя");
+Q.input("конец\nда\n\n");
